354f5d326710a411f496012a6691d835bce141bb
EmilWallett <nhlpool27@gmail.com>
2022-08-29 10:41:26 +0200

    Första test med if/else if
    splegg

---
+++ my name leg/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Är det bra väder idag?");
+        string svar = Console.ReadLine();
+        if(svar == "Ja" || svar == "ja" || svar == "JA")
+        {
+            Console.WriteLine("Kulimojs");
+        }
+        else if(svar == "Nej" || svar == "nej" || svar == "NEJ")
+        {
+            Console.WriteLine("Inte skojsigt");
+        }
+        else if(svar == "308 Negra Arroyo Lane")
+        {
+            Console.WriteLine("I am the one who knocks");
+        }
+        else
+        {
+            Console.WriteLine(svar + " du låter som leg");
+        }
+    }
+}

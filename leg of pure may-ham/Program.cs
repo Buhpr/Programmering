fc1e09c3aeeeee5d38341de2c525a58d16d5d6f4
Buhpr <112383181+Buhpr@users.noreply.github.com>
2022-09-05 10:25:44 +0200

    for funktion nr1
    så cool

---
+++ leg of pure may-ham/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int ham;
+
+            for(ham=1;ham <= 20;ham++)
+        {
+            Console.WriteLine(ham+" " + "leg of ham");
+        }
+    }
+}

9f730b71c7d07bfe3891728393ee6d091e01ecf7
Buhpr <112383181+Buhpr@users.noreply.github.com>
2022-09-05 10:26:24 +0200

    for funktion nr2
    mycket coolare

---
+++ leg with no ham/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ge mig ett tal mellan 1 och 100");
+        
+        for (int ham = Convert.ToInt32(Console.ReadLine()); ham <= 101; ham++)
+        {
+            Console.WriteLine("talet är " + ham);
+        }
+    }
+}

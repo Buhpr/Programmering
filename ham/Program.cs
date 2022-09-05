b059833952418f201f6b7d9601471bce4e47ce44
Buhpr <112383181+Buhpr@users.noreply.github.com>
2022-09-05 08:49:08 +0200

    while projekt
    helgrymt

---
+++ ham/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int Tal;
+        Console.WriteLine("Skriv ett tal mellan 1 och 100");
+        Tal = Convert.ToInt32(Console.ReadLine());
+
+        while(Tal <= 101)
+        {
+            Console.WriteLine(Tal);
+            Tal++;
+        }
+    }
+}

a40a34344bd75ea2f3da2d44422d26af1c68aa65
Buhpr <sika05001@utb.vaxjo.se>
2022-09-12 09:53:20 +0200

    Utveckling av medelvärde
    Mycket cool och helgrym

---
+++ there will be ham/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int antalTal;
+        double summa = 0, input, störst = 0, minst = 99999999999999999; //Det finns säkert ett bättre sätt att göra det här men jag är inte smart nog för att lista ut det.
+        Console.WriteLine("Antal tal som ska matas in?");
+        antalTal = Convert.ToInt32(Console.ReadLine());
+        for (int i=1; i <= antalTal; i++)
+            {
+                Console.WriteLine("Skriv värde " + i + ": ");
+                input = Convert.ToDouble(Console.ReadLine());
+                summa = summa + input;
+                if(input < minst)
+                    {
+                        minst = input;
+                    }
+                if(input > störst)
+                    {
+                        störst = input;
+                    }
+            }
+        Console.WriteLine("Medelvärdet är: " + (summa/antalTal));
+        Console.WriteLine("Minsta talet var: " + minst);
+        Console.WriteLine("Största talet var: " + störst);
+    }
+}

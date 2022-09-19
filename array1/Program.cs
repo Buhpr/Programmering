be6da268d58e93ee9d953ced92b3ce50dbd23c25
Buhpr <sika05001@utb.vaxjo.se>
2022-09-19 10:34:21 +0200

    Array 1 & 2
    Array uppgifter 1 och 2

---
+++ array1/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+   {
+        int[] Leg = new int[5];
+        for (int i = 0; i < Leg.Length; i++)
+        {
+            Console.WriteLine("Ge mig tal nummer " + (i+1) + ": ");
+            Leg [i] = int.Parse(Console.ReadLine());
+        }
+        for(int i = 4; i > -1; i--)
+        {
+            Console.WriteLine(Leg[i]);
+        }
+    }
+}
---
+++ array2/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        double[] temp = {4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9};
+        Console.WriteLine("Medelvärde: " + Medel(temp));
+    }
+
+    static double Medel(double[] a)
+    {
+        double summa = 0;
+        for(int i = 0; i < a.Length; i++)
+        {
+            summa += a[i];
+        }
+        return (Math.Round(summa/ a.Length, 1));
+    }
+}

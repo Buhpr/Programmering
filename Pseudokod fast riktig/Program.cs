6bd781c6155e25eae190c02b0e9c676910b440a6
Buhpr <112383181+Buhpr@users.noreply.github.com>
2022-10-17 08:53:36 +0200

    Random nummer gissning
    Mycket cool och fungerar (tror jag)

---
+++ Pseudokod fast riktig/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rd = new Random();
+        int rand = rd.Next(1, 100);
+        int tal = 0;
+        int försök = 1;
+
+        Console.WriteLine("Skriv ett heltal mellan 1 och 100");
+
+        while(rand != tal)
+        {
+            tal = int.Parse(Console.ReadLine());
+            if(tal == rand)
+            {
+                Console.WriteLine("Det var rätt tal!");
+                Console.WriteLine("Det tog dig " + försök + " försök!");
+            }
+            if(tal < rand)
+            {
+                Console.WriteLine("Talet är för litet, prova igen.");
+                försök ++;
+            }
+            if(tal > rand)
+            {
+                Console.WriteLine("Talet är för stort, prova igen.");
+                försök ++;
+            }
+        }
+    }
+}
